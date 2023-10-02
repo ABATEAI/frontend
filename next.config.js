8cafeb2e319a71721f7340c374dca5eda57a25f7
Jeffry <jeffryklew@gmail.com>
2023-10-02 19:45:21 -0400

    Simplify next.config.js based on codevoweb.com tutorial

--- next.config.js
+++ next.config.js
@@ -1,69 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Route requests starting with /api/google to http://127.0.0.1:8000 server
-    // Route requests starting with /api/square to http://127.0.0.1:8001 server
-    // uvicorn servers are running at both during development
+    // Route /api requests to http://127.0.0.1:8000 uvicorn development server
     // Referenced:
     // - https://vercel.com/guides/
     //   how-to-use-python-and-javascript-in-the-same-application
     // - https://github.com/digitros/nextjs-fastapi/blob/main/next.config.js
+    // - https://github.com/wpcodevo/nextjs-fastapi-framework/
+    //   blob/main/next.config.js
     rewrites: async () => {
         return [
             {
-                source: "/api/google/:path*",
+                source: "/api/:path*",
                 destination:
                     process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/api/google/:path*"
-                        : "/api/google/",
-            },
-            {
-                source: "/google/docs",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/docs"
-                        : "/api/google/docs",
-            },
-            {
-                source: "/google/redoc",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/redoc"
-                        : "/api/google/redoc",
-            },
-            {
-                source: "/google/openapi.json",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/openapi.json"
-                        : "/api/google/openapi.json",
-            },
-            {
-                source: "/api/square/:path*",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8001/api/square/:path*"
-                        : "/api/square/",
-            },
-            {
-                source: "/square/docs",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8001/docs"
-                        : "/api/square/docs",
-            },
-            {
-                source: "/square/redoc",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8001/redoc"
-                        : "/api/square/redoc",
-            },
-            {
-                source: "/square/openapi.json",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8001/openapi.json"
-                        : "/api/square/openapi.json",
+                        ? "http://127.0.0.1:8000/api/:path*"
+                        : "/api/",
             },
         ];
     },
