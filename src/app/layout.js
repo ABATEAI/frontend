1df12767c0c99745588017760ad97f397bf7a973
Jeffry <jeffryklew@gmail.com>
2023-10-09 07:17:09 -0400

    Incorporate react-bootstrap into src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./globals.css"
 import { Inter } from "next/font/google"
+// import { useEffect } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -9,6 +11,23 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  /*
+   * I don't think this is needed since react-bootstrap does not depend on
+   * bootstrap.js and all components are exported on window.ReactBootstrap obj
+   * 
+   * Referenced:
+   * - react-bootstrap.netlify.app/docs/getting-started/introduction/
+   * - react-bootstrap.netlify.app/docs/getting-started/why-react-bootstrap
+   * - blog.logrocket.com/handling-bootstrap-integration-next-js/
+   * - dev.to/anuraggharat/adding-bootstrap-to-nextjs-39b2
+   * - www.slingacademy.com/article/how-to-correctly-use-bootstrap-5-in-next-js/
+   * - nextjs.org/docs/app/api-reference/file-conventions/layout
+   * 
+  useEffect(() => {
+    import("react-bootstrap/dist/react-bootstrap.min.js")
+  }, [])
+  */
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
