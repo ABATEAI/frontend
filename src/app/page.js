d5192b1ec5a402b58da725791126a3092f823310
Jeffry <jeffryklew@gmail.com>
2023-10-13 18:15:31 -0400

    Implement getEndpoint() in src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,20 @@
 import CategoryTabs from "../components/category_tabs"
 import Container from "react-bootstrap/Container"
 
+/**
+ * Get URL to backend endpoint based on development or production environment
+ * @param {string} external_api String with value "square" or "google"
+ * @param {string} endpoint     Path to endpoint (must begin with forward slash)
+ * @returns {string} Environment-dependent URL combining external_api + endpoint
+ */
+function getEndpoint(external_api, endpoint) {
+  if (process.env.NODE_ENV === "development") {
+    return "http://backend:8000/api/" + external_api + endpoint
+  }
+
+  return "https://abateai.com/api/" + external_api + endpoint
+}
+
 export default function Home() {
   return (
     <Container as="main">
