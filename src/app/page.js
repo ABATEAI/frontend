993ca1b0aaa919ef6a516b02dbd8120d49334eec
Jeffry <jeffryklew@gmail.com>
2023-10-13 18:33:47 -0400

    Implement getCatalogSizes() in src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ import Container from "react-bootstrap/Container"
  */
 function getEndpoint(external_api, endpoint) {
   if (process.env.NODE_ENV === "development") {
-    return "http://backend:8000/api/" + external_api + endpoint
+    return "http://localhost:3000/api/" + external_api + endpoint
   }
 
   return "https://abateai.com/api/" + external_api + endpoint
@@ -23,9 +23,7 @@ async function getCatalogObjects() {
   // Referenced
   // - https://nextjs.org/docs/app/building-your-application/data-fetching
   //   /fetching-caching-and-revalidating
-  const res = await fetch(
-    getEndpoint("square", "/catalog_objects", { cache: "force-cache" })
-  )
+  const res = await fetch(getEndpoint("square", "/catalog_objects"))
 
   if (!res.ok) {
     throw new Error("Failed to fetch catalog objects")
@@ -34,12 +32,68 @@ async function getCatalogObjects() {
   return res.json()
 }
 
+/**
+ * Get catalog objs with type ITEM_OPTION from backend (available pizza sizes)
+ * @returns {object} JS object containing JSON from response
+ */
+async function getCatalogSizes() {
+  // Referenced
+  // - https://nextjs.org/docs/app/building-your-application/data-fetching
+  //   /fetching-caching-and-revalidating
+  const res = await fetch(getEndpoint("square", "/sizes"))
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch catalog sizes")
+  }
+
+  return res.json()
+}
+
+/**
+ * Home page of abateai.com
+ * @returns Home page consisting of JSX components
+ */
 export default async function Home() {
-  const catalogObjects = await getCatalogObjects();
+  // Referenced:
+  // - https://nextjs.org/learn/basics/data-fetching/with-data
+
+  // props to pass to CategoryTabs
+  let props = {}
+
+  // Map of category ids to category names
+  let idCategoryMap = new Map()
+
+  // Map of image ids to image names
+  let idImageMap = new Map()
+
+  // Fetch external data at build time
+  try {
+    const catalogObjects = await getCatalogObjects()
+
+    // Parse data from related_objects section of catalogObjects
+    for (const obj of catalogObjects.related_objects) {
+      if (obj.type === "CATEGORY") {
+        idCategoryMap.set(obj.id, obj.category_data.name)
+      } else if (obj.type === "IMAGE") {
+        idImageMap.set(obj.id, obj.image_data.name)
+      }
+    }
+
+    const catalogSizes = await getCatalogSizes()
+
+    props = {
+      idCategoryMap,
+      idImageMap,
+      catalogObjects,
+      catalogSizes,
+    }
+  } catch (error) {
+    console.error("Error:", error)
+  }
 
   return (
     <Container as="main">
-      <CategoryTabs />
+      <CategoryTabs {...props} />
     </Container>
   )
 }
