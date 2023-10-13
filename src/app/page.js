3ce433d7d99767ef72e2c4fac08991ca43a128c0
Jeffry <jeffryklew@gmail.com>
2023-10-13 18:26:20 -0400

    Implement getCatalogObjects() in src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,28 @@ function getEndpoint(external_api, endpoint) {
   return "https://abateai.com/api/" + external_api + endpoint
 }
 
-export default function Home() {
+/**
+ * Get catalog objects from backend (list of pizza sizes excluded)
+ * @returns {object} JS object containing JSON from response
+ */
+async function getCatalogObjects() {
+  // Referenced
+  // - https://nextjs.org/docs/app/building-your-application/data-fetching
+  //   /fetching-caching-and-revalidating
+  const res = await fetch(
+    getEndpoint("square", "/catalog_objects", { cache: "force-cache" })
+  )
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch catalog objects")
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const catalogObjects = await getCatalogObjects();
+
   return (
     <Container as="main">
       <CategoryTabs />
