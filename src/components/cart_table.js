e4a12bdb6ed51c37937511d8b37901a7ee2277bd
Jeffry <jeffryklew@gmail.com>
2023-10-17 23:56:41 -0400

    Implement src/components/cart_table.js

---
+++ src/components/cart_table.js
@@ -0,0 +1,55 @@
+"use client"
+
+import Button from "react-bootstrap/Button"
+import Table from "react-bootstrap/Table"
+import { useEffect } from "react"
+
+export default function CartTable({ cartTable, handleOnDeleteAttempt }) {
+  /*
+   * useEffect() can only be used in client components.
+   * react-bootstrap does not depend on bootstrap.js and
+   * all components are exported on window.ReactBootstrap
+   *
+   * Referenced:
+   * - react-bootstrap.netlify.app/docs/components/buttons/
+   * - react-bootstrap.netlify.app/docs/components/modal/
+   * - react-bootstrap.netlify.app/docs/components/table/
+   * - react-bootstrap.netlify.app/docs/getting-started/introduction/
+   * - react-bootstrap.netlify.app/docs/getting-started/why-react-bootstrap
+   * - blog.logrocket.com/handling-bootstrap-integration-next-js/
+   * - dev.to/anuraggharat/adding-bootstrap-to-nextjs-39b2
+   * - www.slingacademy.com/article/how-to-correctly-use-bootstrap-5-in-next-js/
+   * - nextjs.org/docs/app/api-reference/file-conventions/layout
+   * - nextjs.org/docs/app/building-your-application/rendering/client-components
+   */
+  useEffect(() => {
+    import("react-bootstrap/dist/react-bootstrap.min.js")
+  }, [])
+
+  return (
+    <>
+      <Table borderless hover>
+        <tbody>
+          {[...cartTable.entries()].map((idItemArr) => (
+            <tr id={idItemArr[0]} key={idItemArr[0]}>
+              <td>
+                <Button
+                  onClick={() =>
+                    handleOnDeleteAttempt(idItemArr[0], idItemArr[1])
+                  }
+                >
+                  &times;
+                </Button>
+              </td>
+              <td>{idItemArr[1].name}</td>
+              <td>{idItemArr[1].qty}</td>
+              <td>
+                ${Number(idItemArr[1].qty * idItemArr[1].price).toFixed(2)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  )
+}
